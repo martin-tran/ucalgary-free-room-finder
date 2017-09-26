b008a1fa8f1f205054d23f4dbda00167aca03dcd
Martin Tran <martin.hua.tran@gmail.com>
2017-09-26 13:36:29 -0600

    added working scrapper for faculty of science

--- dbadmin.py
+++ dbadmin.py
@@ -1,8 +1,17 @@
 import sqlite3
 
+DAYS = {day:x for (x, day) in enumerate(['U', 'M', 'T', 'W', 'R', 'F', 'S'])}
 
-conn = sqlite3.connect('room_data.db')
-c = conn.cursor()
+
+def humanize_time(time):
+    time_str = '{:04d}'.format(time, )
+    if time_str[2:] == '25':
+        return '{}:{}'.format(time_str[:2], 15)
+    if time_str[2:] == '50':
+        return '{}:{}'.format(time_str[:2], 30)
+    if time_str[2:] == '75':
+        return '{}:{}'.format(time_str[:2], 45)
+    return '{}:{}'.format(time_str[:2], time_str[2:])
 
 
 def dehumanize_time(time):
@@ -19,81 +28,9 @@ def dehumanize_time(time):
         return int(time[:2] + '50')
     if time[3:] == '45':
         return int(time[:2] + '75')
+    if time[3:] == '50':
+        return int(time[:2]+time[3:])+50
     return int(time[:2] + '00')
-
-
-def add_room(room):
-    """Add a room to the database. Defaults all times as free.
-    
-       Args:
-         room::String - The name of the room to add. Should be in the
-                        format [A-Z]{2-3}[0-9]{2-3}, eg, MS160.
-    """
-    for i in range(0, 2400, 25):
-        timeslots = [(room.upper(), d, i, 0) for d in range(7)]
-        c.executemany('INSERT INTO rooms VALUES (?,?,?,?)', (timeslots))
-    conn.commit()
-
-def add_time(room, day, start, end):
-    """Add a time a room is being used.
-
-       Args:
-         room::String - The name of the room to add.
-         day::Int - The day of the week the room is being used. Weeks start on Sunday.
-         start::Int - The time the room starts being used. Check the README for 
-           details on the format.
-         end::Int - The time the room is done being used.  Check the README for 
-           details on the format.
-    """
-    for i in range(start, end, 25):
-        c.execute('''UPDATE rooms SET taken = 1 WHERE 
-                     room = "{}" AND day = {} AND time = {}'''
-                  .format(room.upper(), day, i))
-    conn.commit()
-    
-def check_room(room, day):
-    """Returns the times a specific room is free for the day.
-    
-       Args:
-         room::String - The name of the room to check.
-         day::Int - The day of the week the room is checked for.
-       Returns:
-         times::[(Int, Int)] - The time intervals the room is free.
-    """
-    times = []
-    for time in c.execute('''SELECT time FROM rooms WHERE room = "{}" AND day = {} AND
-                             taken = 0 ORDER BY time'''.format(room.upper(), day)):
-        times.append((time[0], time[0]+25))
-    return _consolidate_times(times)
-
-
-def find_room(day, start=0, end=2400):
-    """Returns the rooms that are fully or partially available between start and end.
-
-       Args:
-         day::Int - The day of the week the room must be free. Weeks start on Sunday.
-         start::Int - The start time to search for a free room. Check the README for 
-           details on the format.
-         end::Int - The end time to search for a free room. Check the README for 
-           details on the format.
-       Returns:
-         rooms_joined::{String:[(Int, Int)]} - A dictionary mapping rooms to the 
-           times they are free to use.
-    """
-    rooms, rooms_joined = {}, {}
-    for room, time in c.execute('''SELECT room, time FROM rooms WHERE day = {} AND
-                                   time >= {} AND time <= {} AND taken = 0
-                                   ORDER BY room, time'''
-                                .format(day, start, end)):
-        if room not in rooms:
-            rooms[room] = [(time, time+25)]
-        else:
-            rooms[room].append((time, time+25))
-
-    for room, times in rooms.items():
-        rooms_joined[room] = _consolidate_times(times)
-        
-    return rooms_joined
 
 
 def _consolidate_times(times):
@@ -116,3 +53,69 @@ def _consolidate_times(times):
     joined_times.append((start, end))
     return joined_times
 
+
+class DBAdmin():
+
+    def __init__(self):
+        self.conn = sqlite3.connect('room_data.db')
+        self.c = self.conn.cursor()
+
+    def init_table(self):
+        self.conn.execute('CREATE TABLE IF NOT EXISTS rooms('
+                          'room TEXT NOT NULL, '
+                          'day INT, '
+                          'time INT, '
+                          'taken INT DEFAULT 0)')
+
+    def add_room(self, room):
+        """Add a room to the database. Defaults all times as free.
+    
+           Args:
+             room::String - The name of the room to add. Should be in the
+                            format [A-Z]{2-3}[0-9]{2-3}, eg, MS160.
+        """
+        for i in range(0, 2400+1, 25):
+            timeslots = [(room.upper(), d, i, 0) for d in range(7)]
+            self.c.executemany('INSERT INTO rooms VALUES (?,?,?,?)', (timeslots))
+        self.conn.commit()
+
+    def add_time(self, room, days, start, end):
+        """Add a time a room is being used.
+
+           Args:
+             room::String - The name of the room to add.
+             days::String - The days of the week the room is being used. 
+               Weeks start on Sunday.
+             start::Int - The time the room starts being used. Check the README for 
+               details on the format.
+             end::Int - The time the room is done being used.  Check the README for 
+               details on the format.
+        """
+        if not self.c.execute('SELECT EXISTS (SELECT 1 FROM rooms '
+                              'WHERE room="{}" LIMIT 1)'
+                              .format(room,)).fetchone()[0]:
+            self.add_room(room)
+        dehu_start, dehu_end = dehumanize_time(start), dehumanize_time(end)
+        for day in days:
+            for i in range(dehu_start, dehu_end+1, 25):
+                self.c.execute('''UPDATE rooms SET taken = 1 WHERE 
+                                  room = "{}" AND day = {} AND time = {}'''
+                               .format(room.upper(), DAYS[day], i))
+        self.conn.commit()
+
+    def check_room(self, room, day):
+        """Returns the times a specific room is free for the day.
+    
+           Args:
+             room::String - The name of the room to check.
+             day::String - The day of the week the room is checked for.
+           Returns:
+             times::[(Int, Int)] - The time intervals the room is free.
+        """
+        times = []
+        for time in self.c.execute('SELECT time FROM rooms WHERE room '
+                                   '= "{}" AND day = {} AND taken = 0 ORDER BY time'
+                              .format(room.upper(), DAYS[day])):
+            times.append((time[0], time[0]+25))
+        return [(humanize_time(x), humanize_time(y)) for
+                (x, y) in _consolidate_times(times)]
---
+++ scrapper.py
@@ -0,0 +1,46 @@
+import re
+import dbadmin
+
+from urllib import request
+from bs4 import BeautifulSoup as bs
+
+
+URLFILE_SCI = 'course_listing_urls_sci.txt'
+SCIENCE = ('unitis unitis-courses unitis-courses-course-listings collapsed '
+           'odd attached-row details-row collapsed-detail-row')
+TABLE_ID = re.compile('uofc-table-[0-9]{1,3}')
+TABLE_CLASS = [['uofc-table'], ['uofc-table', 'has-details']]
+RE_TIME = re.compile('(?P<days>[UMTWRFS]{1,3}) (?P<start>[0-9]{2}:[0-9]{2})'
+                     '.*(?P<end>[0-9]{2}:[0-9]{2})')
+RE_ROOM = re.compile('(?P<building>[A-Z]{2,4}).*(?P<room_num>[0-9]{3})')
+
+def main():
+    dba = dbadmin2.DBAdmin()
+    dba.init_table()
+    with open(URLFILE_SCI, 'r') as f:
+        for line in [url.rstrip() for url in f]:
+            print('\nScrapping course listings from:', line)
+            soup = bs(request.urlopen(line), 'html.parser')
+            tables = soup.find_all(id=TABLE_ID)
+
+            for table in tables:
+                if table['class'] in TABLE_CLASS:
+                    for class_section in table.find_all('tr'):
+                        if not class_section.find('div'):
+                            tds = list(class_section.children)
+                            time = RE_TIME.search(tds[2].text)
+                            place = RE_ROOM.search(tds[3].text)
+                            if time and place:
+                                print('{}\t{}\t{}\t{}'
+                                      .format(place['building']+place['room_num'],
+                                              time['days'],
+                                              time['start'],
+                                              time['end']))
+                                dba.add_time(place['building']+place['room_num'],
+                                                 time['days'],
+                                                 time['start'],
+                                                 time['end'])
+    dba.conn.close()
+
+if __name__ == '__main__':
+    main()
